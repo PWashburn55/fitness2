4fb1a4024c9854233c884ab6d32ac0765d2b98f7
PWashburn55 <pw325555@gmail.com>
2020-12-18 08:09:09 +0000

    added product details

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 
 def all_products(request):
-    """ to return the products page """
+    """ to show all the products """
     
     products = Product.objects.all()
 
@@ -14,3 +14,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ to show the individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
