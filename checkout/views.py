2d4fb50b45c836fe5dd683cce10b19075e0fb67f
PWashburn55 <pw325555@gmail.com>
2020-12-28 19:50:14 +0000

    updated homepage & image

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,7 @@ import stripe
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    client_secret = settings.STRIPE_SECRET_KEY
 
     if request.method =='POST':
         bag = request.session.get('bag', {})
@@ -72,7 +72,7 @@ def checkout(request):
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
         stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
+        stripe.api_key = client_secret
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
